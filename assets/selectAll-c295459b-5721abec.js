c9981e6cf503f2904717e674f5af3d4542b653ac
Tomáš Horáček <tomas.horacek@gmail.com>
2023-06-22 14:12:43 +0200

    Updates

--- assets/selectAll-c295459b-5721abec.js
+++
@@ -1,1 +0,0 @@
-import{bf as t,bg as n,bh as o}from"./index-8cc6c637.js";function a(e){return typeof e=="string"?new t([document.querySelectorAll(e)],[document.documentElement]):new t([o(e)],n)}export{a as s};
